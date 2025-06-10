86765c2befb76d2a3963acef10f1b69ca4c81ef9
Renato Freitas <dev.renato10@gmail.com>
2025-06-10 18:40:13 -0300

    started to improve readLexeme() to create tokens from lexemes

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 public class Lexer {
 
     public void readLexeme(String input) {
-        ArrayList<String> tokens = new ArrayList<String>();
+
+        LexerVerifier verif = new LexerVerifier();
+
+        ArrayList<Token> tokensList = new ArrayList<Token>();
+        ArrayList<String> symbolTable = new ArrayList<String>();
 
         String[] lines = input.split("\n"); // store each line of the code as an element
         for (int i = 0; i < lines.length; i++){ // format each line removing unnecessary spaces
@@ -20,46 +24,38 @@ public class Lexer {
 
                 end = c;
 
-                if (!isLetterOrDigit(chars[c])){
+                if (!verif.isAlphanumeric(chars[c]) && !verif.isUnderscore(chars[c])){
 
                     // build the previous lexeme (start untiel previous char)
                     // if a non digit or letter folow another non digit ou letter, the start and end pointers will be the same
                     if (start != end){
                         String buildingLexeme = lines[l].substring(start,end); // start is inclusive and end is exclusive
-                        tokens.add(buildingLexeme);
+                        String name = TokenNames.IDENTIFIER.name();
+
+                        if (!verif.isReservedWord(buildingLexeme)){
+                            symbolTable.add(buildingLexeme);
+                            Token tokenIdentifier = new Token(name, symbolTable.indexOf(buildingLexeme));
+                            tokensList.add(tokenIdentifier);
+                        }
+
+//                        tokensList.add(buildingLexeme);
                     }
 
                     // build the current lexeme (current char)
-                    if (!isSpace(chars[c])){
+                    if (!verif.isSpace(chars[c])){
                         char currentLexeme = chars[c];
-                        tokens.add(String.valueOf(currentLexeme));
+//                        tokensList.add(String.valueOf(currentLexeme));
                     }
 
                     start = c+1; // if character is space ou another token, set the initial pointer to the next character
                 }
             }
         }
-        System.out.println(tokens);
-    }
-
-    public boolean isLetterOrDigit(char symbol){
-        int[] ascii = {48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,
-        87,88,89,90,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122};
-
-        for (int a: ascii){
-            if ((int) symbol == a){
-                return true;
-            }
+        System.out.println(symbolTable);
+        for (Token token: tokensList){
+            System.out.println(token.toString());
         }
-        return false;
-    }
-
-    public boolean isSpace(char symbol){
-        return ((int) symbol) == 32;
-    }
-
-    private void isReservedWord(String identifier){
-
+//        System.out.println(tokensList);
     }
 
     private void buildToken(String lexeme){
--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -2,7 +2,30 @@ public class Token {
 
     private String name;
     private String attribute;
+    private int attributeId;
     private int line;
 
+    public Token(String name, String attribute) {
+        this.name = name;
+        this.attribute = attribute;
+    }
 
+    public Token(String name){
+        this.name = name;
+    }
+
+    public Token(String name, int attribute){
+        this.name = name;
+        this.attributeId = attribute;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +
+                "name='" + name + '\'' +
+                ", attribute='" + attribute + '\'' +
+                ", attributeId=" + attributeId +
+                ", line=" + line +
+                '}';
+    }
 }
