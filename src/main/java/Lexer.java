cd0d3562151e14eb067ab2c3eba32877e978de72
Renato Freitas <dev.renato10@gmail.com>
2025-06-10 10:06:09 -0300

    improved the logic of readLexeme()

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -4,40 +4,39 @@ public class Lexer {
 
     public void readLexeme(String input) {
         ArrayList<String> tokens = new ArrayList<String>();
-        int start = 0;
-        int end = 0;
 
-        String[] lines = input.split("\n");
-        for (int i = 0; i < (lines.length); i++){
+        String[] lines = input.split("\n"); // store each line of the code as an element
+        for (int i = 0; i < lines.length; i++){ // format each line removing unnecessary spaces
             lines[i] = lines[i].strip();
         }
 
-        for (int l = 0; l < lines.length; l++){
-            char[] chars = lines[l].toCharArray();
+        for (int l = 0; l < lines.length; l++){ // iterating each line of the code
+            char[] chars = lines[l].toCharArray(); // convert the current line into a char array
 
-            StringBuilder lexeme = new StringBuilder();
-            start = chars[0];
-            end = chars[0];
+            StringBuilder buildingLexeme = new StringBuilder(); // stores the lexeme that has been building
 
-            for (int c = 0; c < chars.length; c++) {
-                if (isCharacterOrDigit(chars[c])) {
-                    lexeme.append(chars[c]);
-                } else {
-                    if (!lexeme.toString().isBlank()){
-                        tokens.add(String.valueOf(lexeme));
-                        lexeme.setLength(0);
-                    } else if (!isSpace(chars[c])) {
-                        lexeme.append(chars[c]);
-                        tokens.add(String.valueOf(lexeme));
-                        lexeme.setLength(0);
+            for (int c = 0; c < chars.length; c++) { // for each character of the line
+
+                if (!isLetterOrDigit(chars[c])) {
+
+                    if (!buildingLexeme.toString().isBlank()){
+                        tokens.add(String.valueOf(buildingLexeme));
+                        buildingLexeme.setLength(0);
                     }
+                    if (!isSpace(chars[c])){
+                        char currentLexeme = chars[c];
+                        tokens.add(String.valueOf(currentLexeme));
+                    }
+
+                } else {
+                    buildingLexeme.append(chars[c]);
                 }
             }
         }
         System.out.println(tokens);
     }
 
-    public boolean isCharacterOrDigit(char symbol){
+    public boolean isLetterOrDigit(char symbol){
         int[] ascii = {48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,
         87,88,89,90,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122};
 
@@ -56,4 +55,9 @@ public class Lexer {
     private void isReservedWord(String identifier){
 
     }
+
+    private void buildToken(String lexeme){
+
+    }
+
 }
